27ee32104939cf672501d7c5ee2a63c244c558f7
Tan Nguyen <74261778+tannlde@users.noreply.github.com>
2022-01-07 22:16:54 +0700

    Update defaut value of Color

--- OOP-Polymorphism/Shape.cs
+++ OOP-Polymorphism/Shape.cs
@@ -4,8 +4,8 @@ namespace OOP_Polymorphism
 {
     abstract class Shape:IShape
     {
-        public String Color { get; set; }
-
+        public String Color { get; set; } = "Blue";
+       
         public virtual void Draw()
         {
             Console.WriteLine("Drawing is not overrided!!!");
